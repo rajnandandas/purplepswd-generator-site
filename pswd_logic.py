c3dafc9bf82bd6283f42f5136fd924ab5df96402
Raj Nandan Das <143824336+rajnandandas@users.noreply.github.com>
2025-12-27 15:40:25 +0000

    All committed

--- pswd_logic.py
+++ pswd_logic.py
@@ -4,11 +4,11 @@ import string
 
 def generate_password(length=12, symbols=True):
     chars = string.ascii_letters + string.digits
-    spc_chars = "!@#$%^&*()"
+    spc_chars = string.punctuation
     if symbols:
         chars += spc_chars
         pswd_final = ""
-        while len(set(pswd_final) & set(spc_chars))==0:
+        while (len(set(pswd_final) & set(spc_chars))==0) or (len(set(pswd_final) & set(string.digits))==0):
             pswd_final = "".join(random.choice(chars) for _ in range(length))
         return pswd_final
     return "".join(random.choice(chars) for _ in range(length))
